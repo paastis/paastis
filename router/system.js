3f9f8db064403e094c66ca79e90c1327dbf51ef1
jbuget <jbuget@gmail.com>
2022-09-15 23:43:12 +0200

    fix: system route

--- router/system.js
+++ router/system.js
@@ -1,14 +1,16 @@
 import config from "../config.js";
 import redis from "../redis.js";
+import registry from "../registry/index.js";
 
 export default async (req, res) => {
-  const givenApiToken = req.headers['PaastisProxySystemApiToken'];
+  const givenApiToken = req.headers['PaastisProxySystemApiToken'.toLowerCase()];
 
   if (givenApiToken === config.routing.systemApiToken) {
-    const data = await redis.keys('*');
+    const data = await registry.listApps();
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ status: 'ok', running_apps: data }));
   } else {
+    res.writeHead(400, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(new Error('Wrong Paastis proxy system API token')));
   }
 }
