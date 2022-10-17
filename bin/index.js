22f98436545cf418316b4c5e2f59c41815e5ad22
jbuget <jbuget@gmail.com>
2022-10-17 13:34:49 +0200

    feat (CLI): make Paastis a possible Node CLI module availaible via npmjs

---
+++ bin/index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+// Cf. https://dev.to/rushankhan1/build-a-cli-with-node-js-4jbi
+
+console.log("🍻 Hello Paastis!");
+
+import config from '../src/config.js';
+import Server from '../src/Server.js';
+import Scheduler from '../src/Scheduler.js';
+
+let proxy = new Server(config);
+let scheduler = new Scheduler(config);
+
+async function main() {
+  await proxy.start();
+  await scheduler.start();
+}
+
+await main();
