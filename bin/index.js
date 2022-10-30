660f9cd195044d3120ef672fdc3df7440ceeab6a
jbuget <jbuget@gmail.com>
2022-10-30 17:50:57 +0100

    feat: add options '--max-idle-time <number>' and '--port <number>'

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,22 @@
-#! /usr/bin/env node
-
-// Cf. https://dev.to/rushankhan1/build-a-cli-with-node-js-4jbi
-
-console.log('🍻 Hello Paastis!');
+#!/usr/bin/env node
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
 
 import config from '../src/config.js';
 import Server from '../src/Server.js';
 import Scheduler from '../src/Scheduler.js';
+
+const argv = yargs(hideBin(process.argv)).argv;
+
+if (argv['max-idle-time']) {
+  const maxIdleTime = +argv['max-idle-time'];
+  config.updateActivityMaxIdleTime(maxIdleTime);
+}
+
+if (argv['port']) {
+  const port = +argv['port'];
+  config.updatePort(port);
+}
 
 let proxy = new Server(config);
 let scheduler = new Scheduler(config);
